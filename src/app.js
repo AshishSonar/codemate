09c2a3f50461d6e5c7ffc6de0365fa79f7718499
AshishSonar <ashish08sonar@gmail.com>
2025-08-03 20:39:07 +0530

    chat functionality

--- src/app.js
+++ src/app.js
@@ -3,13 +3,18 @@ const app = express();
 const connectDB = require("./config/database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const http = require("http");
 require("dotenv").config();
+
 
 const requestRouter = require("./routes/request");
 const profileRouter = require("./routes/profile");
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
 const aiRouter = require("./routes/ai");
+const chatRouter = require("./routes/chat");
+const initialiseSoket = require("./utils/socket");
+
 
 app.use(
   cors({
@@ -26,12 +31,15 @@ app.use("/", profileRouter);
 app.use("/", authRouter);
 app.use("/", userRouter);
 app.use("/", aiRouter);
+app.use("/", chatRouter);
 
+const server = http.createServer(app);
+initialiseSoket(server);
 
 connectDB()
   .then(() => {
     console.log("Database connection established...");
-    app.listen(process.env.PORT, () =>
+    server.listen(process.env.PORT, () =>
       console.log("Server listening on Port 3000")
     );
   })
